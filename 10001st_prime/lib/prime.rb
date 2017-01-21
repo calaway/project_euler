4fcded250b08573fa381d545a63e455e3c1f2b99
Calaway <calaway@users.noreply.github.com>
2017-01-21 14:59:27 -0700

    Optimize is_prime? algorithm for 41% efficiency gain.

--- 10001st_prime/lib/prime.rb
+++ 10001st_prime/lib/prime.rb
@@ -1,8 +1,14 @@
 class Prime
   def is_prime?(number)
-    sqrt = Math.sqrt(number).floor
-    (2..sqrt).all? do |divisor|
-      number % divisor != 0
+    if number == 2
+      true
+    elsif number % 2 == 0
+      false
+    else
+      sqrt = Math.sqrt(number).floor
+      (3..sqrt).step(2).all? do |divisor|
+        number % divisor != 0
+      end
     end
   end
 
@@ -13,7 +19,6 @@ class Prime
     until index == nth do
       dividend += 1
       if is_prime?(dividend)
-        print "#{dividend}, "
         index += 1
       end
     end
